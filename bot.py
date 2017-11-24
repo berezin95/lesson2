fbbb6d0ff70a13f6035bc333d6885064c7cbdf68
Konstantin Berezin <berezin95@mail.ru>
2017-11-24 21:45:02 +0300

    bot.py modification

--- bot.py
+++ bot.py
@@ -21,21 +21,64 @@ def greet_user(bot, update):
 
     print("Вызван")
 
-def chat(bot, update):
+'''def chat(bot, update):
     text = update.message.text
     logging.info(text)
-    update.message.reply_text(text)
+    update.message.reply_text(text)'''
 
-def get_constellation(bot, update):
-    update.message.reply_text('Введи название планеты')
-    text = update.message.text
-    update.message.reply_text(text)
+def get_constellation(bot, update, args):
+    #print(args[0])
+    date = datetime.datetime.now().strftime('%Y/%m/%d')
+    #print(date)
+    #planet = ephem.args[0](date.strftime('%Y/%m/%d'))
+    #print(ephem.constellation(planet))
+
+    if args[0] == 'Mercury':
+        planet = ephem.Mercury(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Venus':
+        planet = ephem.Venus(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+    
+    if args[0] == 'Earth':
+        planet = ephem.Earth(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Mars':
+        planet = ephem.Mars(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Jupiter':
+        planet = ephem.Jupiter(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Saturn':
+        planet = ephem.Saturn(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Uranus':
+        planet = ephem.Uranus(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))
+
+    if args[0] == 'Neptune':
+        planet = ephem.Neptune(date)
+        result = ephem.constellation(planet)
+        update.message.reply_text('{} is in constellation of {}'.format(args[0], result[1]))    
+
 
 def main():
     updater = Updater(settings.TELEGRAM_API_KEY)
  
     updater.dispatcher.add_handler(CommandHandler("start", greet_user))
-    updater.dispatcher.add_handler(CommandHandler("planet", get_constellation))
+    updater.dispatcher.add_handler(CommandHandler("planet", get_constellation, pass_args=True))
     #updater.dispatcher.add_handler(MessageHandler(Filters.text, chat))
     
     updater.start_polling()
